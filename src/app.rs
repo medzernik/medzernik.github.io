af2af78f8fc0ecfa7056974ac73d1b131711cb83
medzernik <2439101-medzernik@users.noreply.gitlab.com>
2025-03-21 11:11:32 +0100

    add router

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,21 @@
 use leptos::prelude::*;
-
 #[component]
 pub fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
-
     view! {
-        <button on:click=move |_| set_count.set(3)>"Click me: " {count}</button>
-        <p>"Double count: " {move || count.get() * 2}</p>
+        <button on:click=move |_| *set_count.write() += 1>"Click me"</button>
+        // now we use our component!
+        <ProgressBar progress=count />
+    }
+}
+
+#[component]
+fn ProgressBar(progress: ReadSignal<i32>) -> impl IntoView {
+    view! {
+        <progress
+            max="50"
+            // now this works
+            value=progress
+        />
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use leptos::prelude::*;
+
 mod app;
+
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(app::App);
