f8c2decd5f26ce9c51de64b1f41e06e49326b37c
medzernik <1900179+medzernik@users.noreply.github.com>
2025-05-21 23:12:23 +0000

    push

--- src/pages/about_page.rs
+++ src/pages/about_page.rs
@@ -1,12 +1,6 @@
-use std::{path::PathBuf, str::FromStr};
-
-use crate::{
-    app::load_md_file,
-    components::header::{Header, TopBar},
-};
+use crate::components::header::{Header, TopBar};
 use leptos::prelude::*;
-
-use leptos_reactive::SignalWith;
+use leptos_reactive::{SignalGet, SignalWith, create_signal};
 use stylance::import_crate_style;
 
 import_crate_style!(style, "src/main.module.css");
@@ -24,27 +18,13 @@ pub fn AboutPage() -> impl IntoView {
 
 #[component]
 pub fn AboutPageContent() -> impl IntoView {
-    let html = leptos_reactive::create_resource(
-        || (),
-        |_| async move {
-            markdown::to_html(
-                &load_md_file(PathBuf::from_str("/public/content/about/about.md").unwrap()).await,
-            )
-        },
-    );
+    let (test, set_test) = signal("# Hello, Markdown!\n\nThis is a **bold** text.");
+    // let (data, set_data) =
+    //     create_signal("# Hello, Markdown!\n\nThis is a **bold** text.".to_string());
 
-    view! {
-        <div class=style::bodyContainer>
-            <Suspense fallback=|| {
-                view! { <p>"Loading..."</p> }
-            }>
-                {move || {
-                    html.with(|data| {
-                        view! { <div class=style::bodyText inner_html=data.clone().unwrap()></div> }
-                    })
-                }},
+    let text = move || "# Hello, Markdown!\n\nThis is a **bold** text.".to_string();
 
-            </Suspense>
-        </div>
-    }
+    // Effect::new(move |_| data.get());
+
+    view! { <div class=style::bodyContainer>{move || { test.get() }}</div> }
 }
