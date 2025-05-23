d6ad3f2d47da9ebe5e9f04a9578a11e3d6f7de86
medzernik <1900179+medzernik@users.noreply.github.com>
2025-05-23 20:13:47 +0000

    finally got some markdown rendering

--- src/pages/about_page.rs
+++ src/pages/about_page.rs
@@ -1,6 +1,7 @@
 use crate::components::header::{Header, TopBar};
-use leptos::prelude::*;
+use leptos::{html::inner_html, prelude::*};
 use leptos_reactive::{SignalGet, SignalWith, create_signal};
+use markdown::to_html;
 use stylance::import_crate_style;
 
 import_crate_style!(style, "src/main.module.css");
@@ -19,12 +20,6 @@ pub fn AboutPage() -> impl IntoView {
 #[component]
 pub fn AboutPageContent() -> impl IntoView {
     let (test, set_test) = signal("# Hello, Markdown!\n\nThis is a **bold** text.");
-    // let (data, set_data) =
-    //     create_signal("# Hello, Markdown!\n\nThis is a **bold** text.".to_string());
 
-    let text = move || "# Hello, Markdown!\n\nThis is a **bold** text.".to_string();
-
-    // Effect::new(move |_| data.get());
-
-    view! { <div class=style::bodyContainer>{move || { test.get() }}</div> }
+    view! { <div class=style::bodyText inner_html=move || to_html(test.get())></div> }
 }
