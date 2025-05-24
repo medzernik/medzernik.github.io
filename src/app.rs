0e9454f12bb6235be3c29aabd2152a4d4a232678
medzernik <1900179+medzernik@users.noreply.github.com>
2025-05-24 15:50:11 +0000

    experiment

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,5 @@
 use crate::pages::{about_page::AboutPage, home_page::HomePage, posts_page::PostsPage};
 use leptos::{attr::Scope, prelude::*};
-use leptos_reactive::provide_context;
 use leptos_router::{components::*, path};
 use stylance::import_crate_style;
 
@@ -12,8 +11,8 @@ pub fn App() -> impl IntoView {
         <Router>
             <Routes fallback=|| view! { <div class=style::headerText>"404 - NOT FOUND"</div> }>
                 <Route path=path!("/") view=HomePage />
-                <Route path=path!("/posts") view=PostsPage />
-                <Route path=path!("/about") view=AboutPage />
+                <Route path=path!("/posts/:id") view=PostsPage />
+                <Route path=path!("/:about") view=AboutPage />
             </Routes>
         </Router>
     }
